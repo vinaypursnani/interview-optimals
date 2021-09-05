c9e4ea1bf8545052a82170c39cd713543192e5d2
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2021-09-06 00:58:14 +0530

    Create sumOfPairs.py

---
+++ sumOfPairs.py
@@ -0,0 +1,16 @@
+# Given an array find values that add up to give the targetSum
+# Provided that the array is dsorted this can be done in
+
+my_hash = {}
+def sumOfPairsOp(array, targetSum):
+	for i in array:
+		if targetSum-i in my_hash:
+			return [i,targetSum-i]
+		else:
+			my_hash[i] = True
+			# print(my_hash)
+	return []
+
+arr = ([-2,-1,1,2,45,55,110,125])
+# print(arr)
+print(sumOfPairsOp(arr,100))
