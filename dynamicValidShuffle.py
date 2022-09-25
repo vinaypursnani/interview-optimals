a1cdb4c963eac00297c24a1f687b5b1d7048583f
Vinay Pursnani <52756604+vinaypursnani@users.noreply.github.com>
2022-09-24 19:11:53 -0500

    Add files via upload
    Given 3 array, check if the 3rd array is valid combined shuffle of array 1 and 2

---
+++ dynamicValidShuffle.py
@@ -0,0 +1,32 @@
+def validShuffle(X, Y, shuffle) -> bool:
+    dp = {}
+    
+    # initialize all the alphabets to 0 in a data structure
+    for index in range(26):
+        dp[ index + ord('A') ] = 0
+
+    # checking if char in X, if yes than updating the value to +1
+    for char in X:
+        dp[ ord(char) ] += 1
+
+    # checking if char in Y, if yes than updating the value to +1
+    for char in Y: 
+        dp[ ord(Y) ] += 1
+        
+    # finding char if char in shuffle and decrementing if char is 
+
+    for char in shuffle:
+        dp[ ord(char) ] -= 1
+
+    for i in range(26):
+        if( dp[ i + ord('A') ] != 0):
+            return False 
+
+    return True
+    
+
+if __name__ == '__main__':
+    a = "DYNAMIC"
+    b = "PROGRAMMING"
+    c = "PRODGYRNAMAMMIINCG"
+    print(main(a, b, c))
